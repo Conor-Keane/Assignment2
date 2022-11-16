539a7a6c02cbe19e7c3e86d9ae4dc8fcaa69ba51
Conor-Keane <g00391846@atu.ie>
2022-11-16 14:50:50 +0000

    Added code to PassengerService class

--- src/main/java/ie/atu/Assignment2/PassengerService.java
+++ src/main/java/ie/atu/Assignment2/PassengerService.java
@@ -1,4 +1,23 @@
 package ie.atu.Assignment2;
 
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Service
 public class PassengerService {
+
+    public Passenger getPassenger(@PathVariable String passengerID)
+    {
+        return new Passenger("Mr", "Conor", "1234", "0879543461", 21);
+    }
+
+    public List<Passenger> getPassengers() {
+        List<Passenger> myPassengers = List.of(
+                new Passenger("Mr", "Conor", "12345678910", "0879543461", 23),
+                new Passenger("Mrs", "Conita", "0879543461", "123456819", 45),
+                new Passenger("Ms", "Con", "0123456789", "246835790", 20));
+        return myPassengers;
+    }
 }
