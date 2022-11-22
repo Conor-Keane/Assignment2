726674aa50ebbcf2a3f63632c1f2d3b4ab8bd60d
conorkeane <g00391846@atu.ie>
2022-11-22 23:18:19 +0000

    Made Changes to PassengerController class

--- src/main/java/ie/atu/Assignment2/PassengerController.java
+++ src/main/java/ie/atu/Assignment2/PassengerController.java
@@ -1,5 +1,6 @@
 package ie.atu.Assignment2;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,21 +13,25 @@ import java.util.List;
 @RequestMapping(path = "api/passenger")
 
 public class PassengerController {
-    public List<Passenger> myPassengers = List.of(
-            new Passenger("Mr", "Conor", "12345678910", "0879543461", 21),
-            new Passenger("Mr", "Conor2", "4567891011", "091098765", 24),
-            new Passenger("Mr", "Conor3", "78910111213", "087123456", 21)
-    );
+    private PassengerService newService;
 
+    public PassengerController(PassengerService newService) {
+        this.newService = newService;
+    }
 
-    @GetMapping
-    public List<Passenger> getMyPassengers() {
-        return myPassengers;
+    public static void main(String[] args) {
+        SpringApplication.run(PassengerController.class, args);
+    }
+
+    @GetMapping("")
+    public List<Passenger> getMyPassengers()
+    {
+        return newService.getPassengers();
     }
 
     @GetMapping("/{passengerID}")
     public Passenger getPassenger(@PathVariable String passengerID) {
-        return new Passenger("Mr", "Conor", "1234", "0879543461", 21);
+        return (Passenger) newService.getPassenger(passengerID);
     }
 
 }
