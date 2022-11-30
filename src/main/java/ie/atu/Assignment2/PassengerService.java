7794c849c8206061debe820e2f1507cc1bd35074
conor-keane <g00391846@atu.ie>
2022-11-30 14:31:32 +0000

    Added interface to access database using JPA Specifications.

--- src/main/java/ie/atu/Assignment2/PassengerService.java
+++ src/main/java/ie/atu/Assignment2/PassengerService.java
@@ -1,13 +1,14 @@
 package ie.atu.Assignment2;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import java.util.List;
-
+@AllArgsConstructor
 @Service
 public class PassengerService {
 
+    private final PassengerRepo passengerRepo;
     public Passenger getPassenger(@PathVariable String passengerID)
 
     {
