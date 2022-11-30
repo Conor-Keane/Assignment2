e05b81df50de289422f117eab6caa41d86382d74
conor-keane <g00391846@atu.ie>
2022-11-30 14:34:31 +0000

    Added getPassenger() method to use passengerRepo and removed hardcoded values

--- src/main/java/ie/atu/Assignment2/PassengerService.java
+++ src/main/java/ie/atu/Assignment2/PassengerService.java
@@ -16,10 +16,6 @@ public class PassengerService {
     }
 
     public List<Passenger> getPassengers() {
-        List<Passenger> myPassengers = List.of(
-                new Passenger("Mr", "Conor", "12345678910", "0879543461", 23),
-                new Passenger("Mrs", "Conita", "0879543461", "123456819", 45),
-                new Passenger("Ms", "Con", "0123456789", "246835790", 20));
-        return myPassengers;
+        return passengerRepo.findAll();
     }
 }
