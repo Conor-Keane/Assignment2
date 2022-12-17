9d82779a476ff34d8edaf44519ae15fc4e4539e4
conor-keane <G00391846>
2022-12-17 01:53:44 +0000

    Added changes to dummy data

--- src/main/java/ie/atu/Assignment2/PassengerService.java
+++ src/main/java/ie/atu/Assignment2/PassengerService.java
@@ -17,11 +17,11 @@ public class PassengerService {
     public List<Passenger> getPassengers(){
         List<Passenger> list = List.of(
                 new Passenger("Mr","Conor","1234567891011", "0879543461", 21),
-                new Passenger("Mrs","Conor","1234567891011", "0879543461", 21),
-                new Passenger("Mr","Conor","1234567891011", "0879543461", 21),
-                new Passenger("Mrs","Conor","1234567891011", "0879543461", 21),
-                new Passenger("Mr","Conor","1234567891011", "0879543461", 21),
-                new Passenger("Mrs","Conor","1234567891011", "0879543461", 21)
+                new Passenger("Mrs","Jessica","1110987654", "1234567891", 24),
+                new Passenger("Ms","Jenna","2356891357913", "2354678796", 27),
+                new Passenger("Mr","Ryan","0987654321011", "0987654321", 40),
+                new Passenger("Mrs","Anna","246810121416", "7531345678", 34),
+                new Passenger("Ms","Paula","0001112233455", "1356785432", 21)
 
         );
         return list;
